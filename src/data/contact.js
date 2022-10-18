f8ce60c3114e04b99dd45803505edb5be6fcf651
Namrata Kulkarni <namratajkulkarni@gmail.com>
2022-10-18 15:55:27 -0400

    first commit

---
+++ src/data/contact.js
@@ -0,0 +1,36 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
+// See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free to add other icons.
+
+const data = [
+  {
+    link: '',
+    label: 'Github',
+    icon: faGithub,
+  },
+  {
+    link: '',
+    label: 'Instagram',
+    icon: faInstagram,
+  },
+  {
+    link: '',
+    label: 'LinkedIn',
+    icon: faLinkedinIn,
+  },
+  {
+    link: '',
+    label: 'Twitter',
+    icon: faTwitter,
+  },
+  {
+    link: 'mailto:namratajkulkarni@gmail.com',
+    label: 'Email',
+    icon: faEnvelope,
+  },
+];
+
+export default data;
---
+++ src/data/routes.js
@@ -0,0 +1,34 @@
+const routes = [
+  {
+    index: true,
+    label: 'NK',
+    path: '/',
+    Image: '',
+  },
+  {
+    label: 'Projects',
+    path: '/projects',
+  },
+  {
+    label: 'Research',
+    path: '/Research',
+  },
+  {
+    label: 'Blogs',
+    path: '/blogs',
+  },
+  {
+    label: 'Resume',
+    path: '/resume',
+  },
+  {
+    label: 'About',
+    path: '/about',
+  },
+  {
+    label: 'Contact',
+    path: '/contact',
+  },
+];
+
+export default routes;
